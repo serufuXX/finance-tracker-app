d5dff2afde87450db385177da8d9c03c36602e94
Tochinskyi Illya <naehk532@gmail.com>
2025-11-14 12:31:02 +0200

    feat : added basic html semantic
    needs to
    a11y
    aria
    Add h2 to sections

--- personal-finance-tracker/src/App.tsx
+++ personal-finance-tracker/src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div>
+          <img src="" alt="Finance Tracker logo" />
+          <h1>Personal Finance Tracker</h1>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#!">2025 Saving</a>
+            </li>
+            <li>
+              <a href="#!">filter</a>
+            </li>
+            <li>
+              <a href="#!">Income Breakdown</a>
+            </li>
+            <li>
+              <a href="#!">Expenses Breakdown</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section aria-label="Something розумне">
+          <p>graphic</p>
+          <p>Income Breakdown graphic</p>
+          <p>Expenses Breakdown graphic</p>
+        </section>
+
+        <section aria-label="Something дуже розумне">
+          <h2>Total saving</h2>
+          <ul>
+            <li>
+              <h3>January</h3>
+              <p>Income</p>
+              <p>Expenses</p>
+              <p>Net</p>
+            </li>
+          </ul>
+        </section>
+
+        <section aria-label="Something дууууже розумне">
+          <table>
+            <caption>Income</caption>
+            <thead>
+              <tr>
+                <th>Select</th>
+                <th>Amount</th>
+                <th>Tags</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Something</td>
+                <td>100000</td>
+                <td>Something</td>
+                <td>10000000000</td>
+              </tr>
+            </tbody>
+          </table>
+          <table>
+            <caption>Expenses</caption>
+            <thead>
+              <tr>
+                <th>Selest</th>
+                <th>Amount</th>
+                <th>Tags</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Something</td>
+                <td>100000</td>
+                <td>Something</td>
+                <td>10000000000</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </main>
+
+      <footer>
+        <p>made serufuXX</p>
+        <p>my social media :</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
